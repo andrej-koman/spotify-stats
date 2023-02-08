8ddd631348888ee132c0b963dae520cdddddefd3
andrej-koman <andrej.koman123@gmail.com>
2023-02-08 14:00:24 +0100

    heroku

--- index.js
+++ index.js
@@ -13,57 +13,58 @@ const REDIRECT_URI = process.env.REDIRECT_URI;
 app.use(cors());
 
 app.get("/login", (req, res) => {
-    const queryParams = querystring.stringify({
-        client_id: process.env.CLIENT_ID,
-        response_type: "code",
-        redirect_uri: process.env.REDIRECT_URI,
-        scope: "user-read-private user-read-email user-read-recently-played user-top-read user-follow-read user-follow-modify playlist-read-private playlist-read-collaborative playlist-modify-public",
-    });
+  const queryParams = querystring.stringify({
+    client_id: process.env.CLIENT_ID,
+    response_type: "code",
+    redirect_uri: process.env.REDIRECT_URI,
+    scope: "user-read-private user-read-email user-read-recently-played user-top-read user-follow-read user-follow-modify playlist-read-private playlist-read-collaborative playlist-modify-public",
+  });
 
-    res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
 
 app.get('/callback', (req, res) => {
-    const code = req.query.code || null;
-  
-    axios({
-      method: 'post',
-      url: 'https://accounts.spotify.com/api/token',
-      data: querystring.stringify({
-        grant_type: 'authorization_code',
-        code: code,
-        redirect_uri: REDIRECT_URI
-      }),
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded',
-        Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
-      },
+  const code = req.query.code || null;
+
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: querystring.stringify({
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: REDIRECT_URI
+    }),
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+    },
+  })
+    .then(response => {
+      if (response.status === 200) {
+        const { access_token, refresh_token, expires_in } = response.data;
+
+        const queryParams = querystring.stringify({
+          access_token,
+          refresh_token,
+          expires_in,
+        });
+
+        res.redirect(`http://localhost:5173/?${queryParams}`);
+
+      } else {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
     })
-      .then(response => {
-        if (response.status === 200) {
-            const { access_token, refresh_token, expires_in } = response.data;
+    .catch(error => {
+      res.send(error);
+    });
+});
 
-            const queryParams = querystring.stringify({
-                access_token,
-                refresh_token,
-                expires_in,
-            });
-
-            res.redirect(`http://localhost:5173/?${queryParams}`);
-            
-        } else {
-            res.redirect('/#' +
-            querystring.stringify({
-              error: 'invalid_token'
-            }));
-        }
-      })
-      .catch(error => {
-        res.send(error);
-      });
-  });
 app.get("/refresh_token", (req, res) => {
-  const {refresh_token} = req.query;
+  const { refresh_token } = req.query;
 
   axios({
     method: 'post',
@@ -86,5 +87,5 @@ app.get("/refresh_token", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
